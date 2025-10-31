0e662f9aa2ccd4a86d62a9e4c3451de9cb167bd0
dougkavanagh <dougkavanagh@gmail.com>
2025-10-31 12:56:27 -0400

    Prettier formatting

--- src/infrastructure/repositories/site-configuration.repository.ts
+++ src/infrastructure/repositories/site-configuration.repository.ts
@@ -115,7 +115,11 @@ export const createSiteConfigurationRepository = ({
     async update(record: UpdateSiteConfiguration) {
       const dbRecord = mapToDbRecordForUpdate(record);
       dbService.updateMetadata(dbRecord);
-      await dbService.update(siteConfig, dbRecord, eq(siteConfig.id, record.id));
+      await dbService.update(
+        siteConfig,
+        dbRecord,
+        eq(siteConfig.id, record.id)
+      );
     },
   };
 };

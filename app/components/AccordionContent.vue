f49bdf001191f6697467c6b3e373393ce95a197f
dkavanagh-oceanmd <92549603+dkavanagh-oceanmd@users.noreply.github.com>
2026-03-17 09:32:56 -0400

    Fix site configuration accordion collapse

--- app/components/AccordionContent.vue
+++ app/components/AccordionContent.vue
@@ -13,8 +13,11 @@ const delegatedProps = computed(() => {
 </script>
 
 <template>
-  <AccordionContent data-slot="accordion-content" v-bind="delegatedProps"
-    class="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm">
+  <AccordionContent
+    data-slot="accordion-content"
+    v-bind="delegatedProps"
+    class="data-[state=closed]:hidden data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm"
+  >
     <div :class="cn('pt-0 pb-4', props.class)">
       <slot />
     </div>
